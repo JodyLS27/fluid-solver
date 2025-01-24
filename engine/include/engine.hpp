ea3406efdd197615b25ecbb9704545a60b5cc91f
Jody Stocks <jodystocks01@gmail.com>
2025-01-24 16:27:21 +0200

    Adding base engine structure - Adding a Namespace: ngine - Adding a class: Engine - Adding the base functions init() and start() with simple docstrings for now

---
+++ engine/include/engine.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace engine
+{
+	class Engine
+	{
+	public:
+		/**
+		* Used to initialize the engine
+		**/
+		void init();
+
+		/**
+		* Start the engine and its main loop
+		**/
+		void start();
+	};
+}
