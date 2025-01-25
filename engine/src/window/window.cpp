2d399b7fb158f3d5a19490ff5bd9a5fa4b01c314
Jody Stocks <jodystocks01@gmail.com>
2025-01-25 22:43:21 +0200

    Updated OpenGL version - Changed the `Open GL` version to `3.3` and not `3.4`, Miss Type

--- engine/src/window/window.cpp
+++ engine/src/window/window.cpp
@@ -8,6 +8,6 @@ void engine::Window::init()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
