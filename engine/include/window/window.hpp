28ea6158393cc5100ed53aad0e6e030fc22b956f
Jody Stocks <jodystocks01@gmail.com>
2025-01-25 21:29:46 +0200

    Adding window header file - Adding `window.hpp` under `engine/incclude/window/`

---
+++ engine/include/window/window.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace engine
+{
+	class Window()
+	{
+	public:
+		/**
+		* Initialize a GLFW window
+		**/
+		void init();
+
+	}
+}
