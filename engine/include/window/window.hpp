9c056682d3ecb6b1d2d4823debd43a8fe41e629b
Jody Stocks <jodystocks01@gmail.com>
2025-02-04 22:21:51 +0200

    Updating window header for new structure - Added a forward declare for `GLFWwindow` - Added a `Window` constructor and destructor - Added a Getter for `GLFWwindow` - Added the `framebugger_size_callback()` function for when windows are resized - Added `process_input()` member function - Added a member variable `m_window` to work within the Window class

--- engine/include/window/window.hpp
+++ engine/include/window/window.hpp
@@ -1,19 +1,44 @@
 #pragma once
 #include <iostream>
 
+struct GLFWwindow;
+
 namespace engine
 {
 	class Window
 	{
 	public:
+		Window();
+		~Window();
+
 		/**
 		* Initialize a GLFW window
 		**/
 		void init();
 
+		/**
+		* Used to return the window object from GLFW
+		**/
+		GLFWwindow* getGLFWwindow() const;
+
+		// --- GLWF Events ---
+
+		/**
+		* called when the window is resized and sets the glViewport to the current window size
+		**/
+		void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+		/**
+		* Used for Input processing like Keyboard, Mouse or controller
+		**/
+		void process_input(GLFWwindow* window);
+
 	private:
 		// Settings
 		const int m_screen_width = 800;
 		const int m_screen_height = 600;
+
+		GLFWwindow* m_window;
+
 	};
 }
