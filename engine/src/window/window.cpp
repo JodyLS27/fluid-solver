73f57ee4f54896d32dc5a1f73e5ba6f7b794db0c
Jody Stocks <jodystocks01@gmail.com>
2025-02-04 22:39:04 +0200

    Base window class setup - Removed Function forward declares - Added a defauly Constructor - Added a Destructor to destroy the GLFW window and Terminate GLFW - Updated `framebuffer_size_callback()` and `proess_input()` to operate withthe current header declaration

--- engine/src/window/window.cpp
+++ engine/src/window/window.cpp
@@ -3,9 +3,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-// Forward Declare
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void process_input(GLFWwindow* window);
+engine::Window::Window() : m_window(nullptr) {}
+
+engine::Window::~Window()
+{
+	glfwDestroyWindow(m_window);
+	glfwTerminate();
+}
+
 
 void engine::Window::init()
 {
@@ -14,65 +19,19 @@ void engine::Window::init()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
-	GLFWwindow* window = glfwCreateWindow(m_screen_width, m_screen_height, "Fluid Solver", NULL, NULL);
-
-	// Crash if the window is null
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window\n";
-		glfwTerminate();
-		//return -1;
-
-	}
-
-	glfwMakeContextCurrent(window);
-
-
-	// Pass GLAD the function to load the address of the OpenGL function pointer which is OS Specific.
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Fail to Initialize GLAD\n";
-		// return -1;
-	}
-
-	// Viewport
-	glViewport(0, 0, m_screen_width, m_screen_height);
-
-	// Event callbacks
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	// Render Loop
-	while (!glfwWindowShouldClose(window))
-	{
-		// Input
-		process_input(window);
-
-		// Rendering
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	// End
-	glfwTerminate();
-	// return 0;
 }
 
 // GLFW Events
 /**
 * Called when resetting a windows size
 **/
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void engine::Window::framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 	std::cout << "\n\n --- Window resized ---\n\n";
 }
 
-void process_input(GLFWwindow* window)
+void engine::Window::process_input(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
