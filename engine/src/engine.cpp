88947c56764f201e43129df11a45f6788cebcbac
Jody Stocks <jodystocks01@gmail.com>
2025-02-04 21:53:06 +0200

    Initial setup - Added `GLAD` and `GLFW` support in the source file - Added TODOS for future thoughts

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -1,12 +1,16 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include "engine.hpp"
+
 
 void engine::Engine::init()
 {
 	window.init();
-
+	// TODO: Get back the window object
 }
 
 void engine::Engine::start()
 {
-
+	// TODO: Start the main loop
 }
