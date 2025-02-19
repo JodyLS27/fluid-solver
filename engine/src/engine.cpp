43ac908f969bb0c6d4da89fa3cb79a4363f981bf
Jody Stocks <jodystocks01@gmail.com>
2025-02-19 08:54:13 +0200

    Prepping to render rectangle - Updating the `vertices[]` array - Adding the `indices[]` array for rendering order

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -100,9 +100,16 @@ int engine::Engine::start()
 	// --- Vertex Data ---
 	float vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		0.5f, 0.5f, 0.0f,   // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f, 0.5f, 0.0f   // top left
+	};
+
+	unsigned int indices[] =
+	{
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
 	};
 
 	// Setup Vertex Buffer Object
